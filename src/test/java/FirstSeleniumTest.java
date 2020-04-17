3681bcdb45d60f8d38a02f6d867936994c058c0b
Your Name <you@example.com>
2020-04-17 12:10:59 +0200

    maximize windows for tests

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,7 +9,9 @@ public class FirstSeleniumTest {
     public void googleOpenTest(){
         String driverpatch = "C:\\Users\\Admin\\IdeaProjects\\SeleniumWebdriverKursUsemy\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",driverpatch);
+        //Dimension dimension = new Dimension(500, 500);
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.google.pl/");
     }
 }
