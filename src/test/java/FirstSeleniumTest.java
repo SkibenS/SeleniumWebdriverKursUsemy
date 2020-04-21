ee626c298ebfb7cc10c8afcf887a799ff01a6948
Your Name <you@example.com>
2020-04-21 11:58:29 +0200

    set systemproperty: webdriver

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,22 +1,20 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
 
 public class FirstSeleniumTest {
 
     @Test
-    public void googleOpenTest(){
+    public void googleOpenTest() {
         String driverpatch = "C:\\Users\\Admin\\IdeaProjects\\SeleniumWebdriverKursUsemy\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver",driverpatch);
-        //Dimension dimension = new Dimension(500, 500);
+        System.setProperty("webdriver.chrome.driver", driverpatch);
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("C:\\Users\\Admin\\Desktop\\Skibi\\KursSeleniumWebDriver\\PlikiPotrzebneDoKursu\\test.html");
-        driver.findElement(By.id("newPage")).click();
-        // driver.close(); // metoda close zamyka okno na który jest focus
-        driver.quit();  // metoda quit zamyka wszystkie okna
+        
     }
 }
 
