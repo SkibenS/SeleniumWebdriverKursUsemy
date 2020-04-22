a52451ec19f9ee8879a13512b5d234c4efe3e1f1
Your Name <you@example.com>
2020-04-22 10:42:39 +0200

    Przełączanie okna przeglądarki

---
+++ src/test/java/PrzelaczanieOknaPrzegladarki.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+
+public class PrzelaczanieOknaPrzegladarki {
+
+    @Test
+    public void googleOpenTest() {
+        String driverpatch = "C:\\Users\\Admin\\IdeaProjects\\SeleniumWebdriverKursUsemy\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverpatch);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("C:\\Users\\Admin\\Desktop\\Skibi\\KursSeleniumWebDriver\\PlikiPotrzebneDoKursu\\test.html");
+
+        WebElement newPageButton = driver.findElement(By.id("newPage"));
+        String currentWindowName = driver.getWindowHandle();
+        newPageButton.click();
+        switchToNewWindow(driver,currentWindowName);
+        System.out.println("Tytuł strony to :" + driver.getTitle());
+        System.out.println("To jest nas obecny URL: " + driver.getCurrentUrl());
+    }
+
+    private void switchToNewWindow(WebDriver driver, String currentWindowName) {
+        System.out.println("Wartość dla obecnego okna to :" + currentWindowName);
+        Set<String> windows  = driver.getWindowHandles();
+        System.out.println("Ilość okien przeglądarki :" + windows.size());
+
+        for (String window : windows) {
+            if (!window.equals(currentWindowName)) {
+                driver.switchTo().window(window);
+            }
+        }
+    }
+
+}
