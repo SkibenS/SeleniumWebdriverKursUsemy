3c36dbe68556f34c5f103414977a6d12a62c74ed
Your Name <you@example.com>
2020-04-22 10:49:23 +0200

    Klikanie na elementy

---
+++ src/test/java/KlikanieNaElementy.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class KlikanieNaElementy{
+
+    @Test
+    public void googleOpenTest() {
+        String driverpatch = "C:\\Users\\Admin\\IdeaProjects\\SeleniumWebdriverKursUsemy\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverpatch);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("C:\\Users\\Admin\\Desktop\\Skibi\\KursSeleniumWebDriver\\PlikiPotrzebneDoKursu\\test.html");
+
+        WebElement clickOnMeButton = driver.findElement(By.id("clickOnMe"));
+        clickOnMeButton.click();
+
+    }
+}
