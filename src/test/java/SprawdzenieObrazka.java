a561bcd4fb33ba17c0cee394281fa113fd364818
Your Name <you@example.com>
2020-04-22 11:19:12 +0200

    sprawdzenie obrazka

---
+++ src/test/java/SprawdzenieObrazka.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class SprawdzenieObrazka{
+
+    @Test
+    public void sprawdzenieObrazka() {
+        String driverpatch = "C:\\Users\\Admin\\IdeaProjects\\SeleniumWebdriverKursUsemy\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverpatch);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("C:\\Users\\Admin\\Desktop\\Skibi\\KursSeleniumWebDriver\\PlikiPotrzebneDoKursu\\test.html");
+
+        WebElement smileImages = driver.findElement(By.id("smileImage"));
+        System.out.println(smileImages.getSize().getHeight());
+        System.out.println(smileImages.getAttribute("naturalHeight"));
+
+
+
+
+    }
+}
