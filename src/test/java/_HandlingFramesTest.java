b46d8c7998c137d4016f0146c8686b8b092f23dd
Your Name <you@example.com>
2020-04-24 11:57:48 +0200

    iframe test

--- src/test/java/_HandlingFramesTest.java
+++ src/test/java/_HandlingFramesTest.java
@@ -1,9 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class _HandlingFramesTest {
+import java.util.List;
+
+public class _HandlingFramesTest extends _BaseSeleniumTest{
 
     @Test
     public void handlingFramesTest() {
-    }
 
+        driver.get("file:///C:/Users/Admin/Desktop/Skibi/KursSeleniumWebDriver/PlikiPotrzebneDoKursu/iFrameTest.html");
+        driver.switchTo().frame(0);
+
+
+
+        WebElement clickOnMeButton = driver.findElement(By.id("clickOnMe"));
+        clickOnMeButton.click();
+        driver.switchTo().alert().accept();
+        driver.switchTo().defaultContent();
+
+        List <WebElement> headers = driver.findElements(By.tagName("h1"));
+        System.out.println(headers.size());
+        System.out.println(headers.get(0).getText());
+
+    }
 }
