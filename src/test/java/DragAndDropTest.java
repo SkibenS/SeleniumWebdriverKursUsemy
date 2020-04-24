f97b52cb446bee6746f1d615208b4d103cf9e8b0
Your Name <you@example.com>
2020-04-24 13:24:36 +0200

    drag and drop test

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DragAndDropTest extends _BaseSeleniumTest {
+
+    @Test
+    public void dragAndDropTest () {
+
+        driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        WebElement elementToDrag = driver.findElement(By.id("draggable"));
+        WebElement dropZone = driver.findElement(By.id("droptarget"));
+
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(elementToDrag,dropZone).build().perform();
+
+    }
+}
--- src/test/java/_GoogleSearchTest.java
+++ src/test/java/_GoogleSearchTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -12,6 +13,11 @@ public class _GoogleSearchTest extends  _BaseSeleniumTest{
         driver.get("https://www.google.pl/");
         WebElement googleSearchbox = driver.findElement(By.name("q"));
         googleSearchbox.sendKeys("Selenium");
+        // Kliknięcie prawym przycoskiem myszy
+
+        Actions action = new Actions(driver);
+        action.contextClick().build().perform();
+
         googleSearchbox.sendKeys(Keys.ENTER);
         driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/a/h3")).click();
         Assert.assertEquals(driver.getTitle(),"SeleniumHQ Browser Automation");
