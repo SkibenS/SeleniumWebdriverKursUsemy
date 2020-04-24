f06ae884aca4726c7e99115254891792d1b68d8d
Your Name <you@example.com>
2020-04-24 12:31:49 +0200

    Hover test

---
+++ src/test/java/HoverTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class HoverTest extends _BaseSeleniumTest{
+
+    @Test
+    public void howerTest() {
+
+        driver.get("https://www.w3schools.com/");
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.id("navbtn_tutorials"))).build().perform();
+    }
+}
