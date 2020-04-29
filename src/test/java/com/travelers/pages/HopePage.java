84854c1c6941faa7167f2a78bc9f6ea55be05f0e
Your Name <you@example.com>
2020-04-29 11:27:03 +0200

    packages for com.travelers pages continues testing

--- src/test/java/com/travelers/pages/HopePage.java
+++ src/test/java/com/travelers/pages/HopePage.java
@@ -1,18 +1,45 @@
 package com.travelers.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.nio.file.WatchEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class HopePage {
 
     @FindBy(xpath = "//span[text()='Search by Hotel or City Name']")
     private WebElement searchSpan;
 
-    @FindBy(xpath = "//div[@id='select2-drop']//input")
+    @FindBy(xpath = "//div[@id='select2-drop']//input") //table[@class='class="bgwhite table table-striped']
     private WebElement searchCityInput;
+
+    @FindBy(name = "checkin")
+    private WebElement checkInInput;
+
+    @FindBy(name = "checkout")
+    private WebElement checkOutInput;
+
+    @FindBy(id = "travellersInput")
+    private WebElement travellersInput;
+
+    @FindBy(id = "adultPlusBtn")
+    private WebElement adultPlusButton;
+
+    @FindBy(id = "childPlusBtn")
+    private WebElement childPlusButton;
+
+    @FindBy(xpath = "//button[text()=' Search']")
+    private WebElement performSearchButton;
+
+    @FindBy(xpath = "//table[@class='bgwhite table table-striped']")
+    private WebElement resultsTable;
 
     public HopePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -25,6 +52,54 @@ public class HopePage {
         searchCityInput.sendKeys(Keys.ENTER);
     }
 
+    public void setDateRange (String checkInDate, String checkOutDate) {
+        checkInInput.sendKeys(checkInDate);
+        checkOutInput.sendKeys(checkOutDate);
+        checkOutInput.click();
+    }
 
+    public void openTravellersInput () throws InterruptedException {
+        Thread.sleep(2000);
+        travellersInput.click();
+    }
 
+    public void addAdultPerson() {
+        adultPlusButton.click();
+    }
+
+    public void addChildPerson() {
+        childPlusButton.click();
+    }
+
+    public void performSearchButton() {
+        performSearchButton.click();
+    }
+
+    public List<String> getHotelNames() throws InterruptedException {
+        List<String> hotelNames = new ArrayList<>();
+        Thread.sleep(3000);
+        List<WebElement> hotelNamesWebElements = resultsTable.findElements(By.xpath("//h4//b"));
+        for (WebElement hotelNameElement : hotelNamesWebElements) {
+            System.out.println(hotelNameElement.getText());
+            hotelNames.add((hotelNameElement.getText()));
+        }
+        return hotelNames;
+    }
+
+    public List<String> getHotelPrices() throws InterruptedException {
+        List<String> hotelPrices = new ArrayList<>();
+        Thread.sleep(3000);
+        List<WebElement> hotelPricesWebElements = resultsTable.findElements(By.xpath("//div[contains(@class,'price_tab')]//b"));
+        for (WebElement hotelPriceElement: hotelPricesWebElements) {
+            System.out.println(hotelPriceElement.getText());
+            hotelPrices.add(hotelPriceElement.getText());
+        }
+        return hotelPrices;
+    }
+
+/*  public List<String> getHotelPrices() {
+        List<WebElement> hotelPrices = resultsTable.findElements(By.xpath("//div[contains(@class,'price_tab')]//b"));
+        List <String> prices = hotelPrices.stream().map(element -> element.getText()).collect(Collectors.toList());
+        return prices;
+    } */
 }
--- src/test/java/com/travelers/tests/SearchHotelTest.java
+++ src/test/java/com/travelers/tests/SearchHotelTest.java
@@ -1,18 +1,41 @@
 package com.travelers.tests;
 
 import com.travelers.pages.HopePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SearchHotelTest extends _BaseSeleniumTest{
 
     @Test
-    public void searchHotelTest () throws InterruptedException {
+    public void setCityHotel() throws InterruptedException {
         driver.manage().timeouts().implicitlyWait(15L, TimeUnit.SECONDS);
         driver.get("http://www.kurs-selenium.pl/demo/");
         HopePage hopePage = new HopePage(driver);
         hopePage.sendKeysToCityInput("Dubai");
+        hopePage.setDateRange("01/05/2020", "08/05/2020");
+        hopePage.openTravellersInput();
+        hopePage.addAdultPerson();
+        hopePage.addChildPerson();
+        hopePage.performSearchButton();
+
+        List <String> hotelName = hopePage.getHotelNames();
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelName.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelName.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelName.get(2));
+
+        List<String> hotelPrice = hopePage.getHotelPrices();
+        Assert.assertEquals("$22", hotelPrice.get(0));
+        Assert.assertEquals("$50", hotelPrice.get(1));
+        Assert.assertEquals("$80", hotelPrice.get(2));
+        /*
+        List<String> prices = hopePage.getHotelPrices();
+        Assert.assertEquals("$22", prices.get(0));
+        Assert.assertEquals("$50", prices.get(1));
+        Assert.assertEquals("$80", prices.get(2));
+         */
 
     }
 }
